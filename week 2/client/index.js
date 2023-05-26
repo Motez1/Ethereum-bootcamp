343b4ae8d4cd6de5732c40fa4ba313f39bb4b503
= <=>
2023-05-26 01:38:09 +0100

    Gift list checker on with using just 32bit

---
+++ week 2/client/index.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+const process = require("process")
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list?
+  const name = process.argv[2];
+  const index = niceList.findIndex(n => n === name);
+  const proof = new MerkleTree(niceList).getProof(index);
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    // TODO: add request body parameters here!
+    proof: proof,
+    name: name
+  });
+
+  console.log({ gift });
+}
+
+main();
